d550b908b781212ba1144750636ca14b4b5bd592
Arvind <arvind.kumar@discounttire.com>
2024-01-31 18:51:33 +0530

    optimiz3ed

--- src/hooks/useResData.js
+++ src/hooks/useResData.js
@@ -6,46 +6,47 @@ const useResData = (API_URL) => {
 
     // use useEffect for one time call getRestaurants using empty dependency array
     useEffect(() => {
-        getRestaurants();
-    }, [getRestaurants]);
+        // async function getRestaurant to fetch API data
+        async function getRestaurants() {
+            // handle the error using try... catch
+            try {
+                const response = await fetch(API_URL);
+                // if response is not ok then throw new Error
+                if (!response.ok) {
+                    const err = response.status;
+                    throw new Error(err);
+                } else {
+                    const json = await response.json();
 
-    // async function getRestaurant to fetch API data
-    async function getRestaurants() {
-        // handle the error using try... catch
-        try {
-            const response = await fetch(API_URL);
-            // if response is not ok then throw new Error
-            if (!response.ok) {
-                const err = response.status;
-                throw new Error(err);
-            } else {
-                const json = await response.json();
+                    // initialize checkJsonData() function to check Swiggy Restaurant data
+                    async function checkJsonData(jsonData) {
+                        for (let i = 0; i < jsonData?.data?.cards.length; i++) {
 
-                // initialize checkJsonData() function to check Swiggy Restaurant data
-                async function checkJsonData(jsonData) {
-                    for (let i = 0; i < jsonData?.data?.cards.length; i++) {
+                            // initialize checkData for Swiggy Restaurant data
+                            let checkData = json?.data?.cards[i]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
 
-                        // initialize checkData for Swiggy Restaurant data
-                        let checkData = json?.data?.cards[i]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
-
-                        // if checkData is not undefined then return it
-                        if (checkData !== undefined) {
-                            return checkData;
+                            // if checkData is not undefined then return it
+                            if (checkData !== undefined) {
+                                return checkData;
+                            }
                         }
                     }
+
+                    // call the checkJsonData() function which return Swiggy Restaurant data
+                    const resData = await checkJsonData(json);
+
+                    // update the state variable restaurants with Swiggy API data
+                    setAllRestaurants(resData);
+                    setFilteredRestaurants(resData);
                 }
-
-                // call the checkJsonData() function which return Swiggy Restaurant data
-                const resData = await checkJsonData(json);
-
-                // update the state variable restaurants with Swiggy API data
-                setAllRestaurants(resData);
-                setFilteredRestaurants(resData);
+            } catch (error) {
+                console.error(error); // show error in console
             }
-        } catch (error) {
-            console.error(error); // show error in console
         }
-    }
+        getRestaurants();
+    }, []);
+
+
     return [allRestaurants, filteredRestaurants]; // return allRestaurants & filteredRestaurants data
 };
 
